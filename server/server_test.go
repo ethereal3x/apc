4ac41d45c21436d02a631594a38c339635e50454
lihuihuang <lihuihuang@9377.com>
2026-05-31 23:52:34 +0800

    feat:启动修复

--- server/server_test.go
+++ server/server_test.go
@@ -6,6 +6,6 @@ func TestRunServer(t *testing.T) {
 	rs := NewRpcServer()
 	hs := NewHttpServer()
 	rs.SetAddress(":8888")
-	hs.SetAddress(":8888")
+	hs.SetAddress(":9999")
 	RunGrpcGatewayService(rs, hs)
 }
